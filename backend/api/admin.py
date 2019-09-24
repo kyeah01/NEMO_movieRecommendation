240f28169f60a2cedc1ae0e86e95ea77683cd510
kyeah01 <kyeah01@gmail.com>
2019-09-24 18:05:37 +0900

    190923 | poster_url

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from .models import Profile, Movie, Rating, ClusterModel
 
 # Register your models here.
+class MovieAdmin(admin.ModelAdmin):
+    fields = ['id', 'title', 'genres', 'group']
+
 admin.site.register(Profile)
-admin.site.register(Movie)
+admin.site.register(Movie, MovieAdmin)
 admin.site.register(Rating)
 admin.site.register(ClusterModel)
--- data/movie-lens100K/movie.py
+++ data/movie-lens100K/movie.py
@@ -1,17 +1,38 @@
-def genre_make():
-    with open('u.genre', 'r', encoding="utf-8") as p:
-        res = {}
-        for line in p.readlines():
-            value, key = line.split('|')
-            res[key if key=='18' else key[:-1]] = value
-    print(res)
+import csv
 
-def movie_make():
-    with open('u.item', 'rb') as p:
+def genre_make():
+    with open('u.genre', 'r', encoding="UTF-8") as p:
         res = []
         for line in p.readlines():
-            
+            value, key = line.split('|')
+            res += [value]
+    return res
+
+def poster_url():
+    with open('movie_poster.csv', 'r') as csvfile:
+        reader = csv.reader(csvfile, quotechar=',')
+        res = {}
+        for row in reader:
+            res[row[0]] = row[1]
+    return res
+
+def movie_make(genre_name, poster):
+    with open('u.item', 'r', encoding = "ISO-8859-1") as p:
+        res = []
+        for line in p.readlines():
+            # ['1682', 'Scream of Stone (Schrei aus Stein) (1991)', '08-Mar-1996', '', 'http://us.imdb.com/M/title-exact?Schrei%20aus%20Stein%20(1991)', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0\n']
+            movie = line.split('|')[:-1]
+            movie = {
+                'id' : movie[0],
+                'title' : movie[1],
+                'date' : movie[2],
+                'genres' : [genre_name[i] for i in range(len(movie[5:])) if int(movie[5+i])],
+                'poster_url' : poster.get(movie[0])
+            }
+            print(movie)
         
 
 if __name__ == '__main__':
-    genre_make()
+    genres = genre_make()
+    poster = poster_url()
+    movie_make(genres, poster)
