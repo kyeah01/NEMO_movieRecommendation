dba913524ae24cbd5f837f2a231b36fcbe4c315a
kyeah01 <kyeah01@gmail.com>
2019-09-24 18:06:51 +0900

    190924 | model에 들어가는 data수정 후 parsing

--- data/movie-lens100K/movie.py
+++ data/movie-lens100K/movie.py
@@ -1,10 +1,15 @@
 import csv
+import requests
+import json
+
+API_URL = 'http://localhost:8000/api/'
+headers = {'content-type': 'application/json'}
 
 def genre_make():
     with open('u.genre', 'r', encoding="UTF-8") as p:
         res = []
         for line in p.readlines():
-            value, key = line.split('|')
+            value = line.split('|')[0]
             res += [value]
     return res
 
@@ -17,22 +22,62 @@ def poster_url():
     return res
 
 def movie_make(genre_name, poster):
+    request_data = {'movies': []}
     with open('u.item', 'r', encoding = "ISO-8859-1") as p:
-        res = []
         for line in p.readlines():
-            # ['1682', 'Scream of Stone (Schrei aus Stein) (1991)', '08-Mar-1996', '', 'http://us.imdb.com/M/title-exact?Schrei%20aus%20Stein%20(1991)', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0\n']
             movie = line.split('|')[:-1]
             movie = {
-                'id' : movie[0],
+                'id' : int(movie[0]),
                 'title' : movie[1],
                 'date' : movie[2],
                 'genres' : [genre_name[i] for i in range(len(movie[5:])) if int(movie[5+i])],
                 'poster_url' : poster.get(movie[0])
             }
-            print(movie)
-        
+            request_data['movies'].append(movie)
+    response = requests.post(API_URL + 'movies/', data=json.dumps(request_data), headers=headers)
+
+def create_user():
+    request_data = {'profiles': []}
+    with open('u.user', 'r', encoding="UTF-8") as p:
+        for line in p.readlines():
+            userid, age, gender, occupation, zip_code = line.split('|')
+            username = 'user' + userid
+            age = int(age)
+            request_data['profiles'].append({
+                'username': username,
+                'password': username,
+                'age': age,
+                'gender': gender,
+                'occupation': occupation,
+                'zip_code' : zip_code,
+                'group' : 1
+            })
+
+
+    response = requests.post(API_URL + 'auth/signup-many/', data=json.dumps(request_data), headers=headers)
+    # print(response.text)
+
+def rating_create():
+    request_data = {'ratings': []}
+    with open('u.data', 'r', encoding="UTF-8") as p:
+        for line in p.readlines():
+            [UserID, MovieID, Rating, Timestamp] = line.split('\t')
+            request_data['ratings'].append({
+                'userid': int(UserID),
+                'movieid': int(MovieID),
+                'rating': int(Rating),
+                'timestamp': Timestamp[:-1]
+            })
+
+    response = requests.post(API_URL + 'ratings/', data=json.dumps(request_data), headers=headers)
+    # print(response.text)
+
+def setupCluster():
+    requests.post(API_URL + 'cluster/', headers=headers)
 
 if __name__ == '__main__':
-    genres = genre_make()
-    poster = poster_url()
-    movie_make(genres, poster)
+    # genres = genre_make()
+    # movie_make(genres, poster_url())
+    # create_user()
+    rating_create()
+    # setupCluster()
