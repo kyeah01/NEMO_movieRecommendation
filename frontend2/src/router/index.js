496b1493ce6dd6f131dc143afb7efd321638c09f
김구현 <siva3020@naver.com>
2019-09-25 09:53:34 +0900

    190925 || Frontend2

---
+++ frontend2/src/router/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import SignPage from '@/views/SignPage'
+import MainPage from '@/views/MainPage'
+import MoviePage from '@/views/MoviePage'
+import ProfilePage from '@/views/ProfilePage'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    { path: '/', component: MainPage, name: 'Home' },
+    { path: '/sign', component: SignPage, name: 'Sign' },
+    { path: '/movie', component: MoviePage, name: 'Movie' },
+    { path: '/profile', component: ProfilePage, name: 'Profile' },
+  ],
+  scrollBehavior() {
+    return { x: 0, y: 0 }
+  },
+})
+
+export default router
---
+++ frontend2/vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: `@import "@/styles/_variables.scss";
+                      @import "@/styles/_navs.scss"; 
+                      @import "@/styles/_spacing.scss"; 
+                      @import "@/styles/_btn.scss"; 
+                      @import "@/styles/_shared-styles.scss";`
+      }
+    }
+  }
+};
