77da431a05912ee209e2ced228a70f2ce7521f76
kyeah01 <kyeah01@gmail.com>
2019-09-30 17:52:15 +0900

    190930 | S1P2233014-7 | rating patch url, object serializer로 수정하게 수정

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,14 +3,20 @@ from django.urls import path
 from api.views import movie_views, rating_views, auth_views, clustering_views
 
 urlpatterns = [
+    # 초기 setup을 위한 url
     url('auth/signup-many/$', auth_views.signup_many, name='sign_up_many'),
     url('auth/signup/$', auth_views.signup, name='sign_up'),
-    url('movies/$', movie_views.movies, name='movie_list'),
     url('rating_many/$', rating_views.rating_many, name='rating_list'),
+    url('cluster/', clustering_views.setup, name="cluster_setup"),
+
+    url('movies/$', movie_views.movies, name='movie_list'),
+    
+    url('rating/$', rating_views.rating, name='rating'),
+
     path('profile/<int:user_id>', auth_views.profile, name='profile'),
     url('auth/login', auth_views.userLogin, name='login'),
     url('auth/logout', auth_views.userLogout, name='logout'),
+
     url('cluster/user', clustering_views.cluster_user_method, name="cluster_user_method"),
     url('cluster/movie', clustering_views.cluster_movie_method, name="cluster_movie_method"),
-    url('cluster/', clustering_views.setup, name="cluster_setup"),
 ]
--- backend/api/views/rating_views.py
+++ backend/api/views/rating_views.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import api_view
 from api.models import Rating
@@ -20,14 +21,30 @@ def rating_many(request):
 
         return Response(status=status.HTTP_200_OK)
 
-@api_view(['POST'])
+@api_view(['POST', 'PATCH'])
 def rating(request):
     if request.method == "POST":
-        rating = request.data.get('rating')
-        userid = rating.get('userid')
-        movieid = rating.get('movieid')
-        rate_value = rating.get('rate')
-        timestamp = round(datetime.now().timestamp())
-        Rating(movie_id=movieid, user_id=userid, rating=rate_value, rating_date=timestamp).save()
+        serializer = RatingSerializer(data=request.data)
+        if serializer.is_valid():
+            rating = serializer.save(rating_date = round(datetime.now().timestamp()))
+            return Response(RatingSerializer(rating).data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    if request.method == "PATCH":
+        id = request.data.get('id')
+        rating = get_object_or_404(Rating, id=id)
+        serializer = RatingSerializer(rating, data=request.data, partial=True)
+        if serializer.is_valid():
+            rating = serializer.save(rating_date = round(datetime.now().timestamp()))
+            return Response(RatingSerializer(rating).data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    return Response(status=status.HTTP_200_OK)
+
+    #     rating = request.data.get('rating')
+    #     userid = rating.get('userid')
+    #     movieid = rating.get('movieid')
+    #     rate_value = rating.get('rate')
+    #     timestamp = round(datetime.now().timestamp())
+    #     Rating(movie_id=movieid, user_id=userid, rating=rate_value, rating_date=timestamp).save()
+
+    # return Response(status=status.HTTP_200_OK)
