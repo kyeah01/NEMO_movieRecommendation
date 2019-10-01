2ba510d519457725b32c66af6c5f97f7cee1d0bf
김구현 <siva3020@naver.com>
2019-10-02 02:00:59 +0900

    191002 || 프로필 수정

---
+++ frontend2/src/components/profile/UserCard.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+                
+    </div>
+</template>
+
+<script>
+export default {
+    name : UserCard,
+    data() {
+        return{
+            
+        }
+    }
+}
+</script>
